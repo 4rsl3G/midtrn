8130763aa37358c72967414e3a5ef696561610a3
Jhody Pedia <53709496+4rsl3G@users.noreply.github.com>
2026-01-05 07:27:16 +0700

    Update midtrans.js

--- midtrans.js
+++ midtrans.js
@@ -2,7 +2,8 @@ import midtransClient from "midtrans-client";
 
 export const coreApi = new midtransClient.CoreApi({
   isProduction: process.env.MIDTRANS_IS_PRODUCTION === "true",
-  serverKey: process.env.MIDTRANS_SERVER_KEY
+  serverKey: process.env.MIDTRANS_SERVER_KEY,
+  clientKey: process.env.MIDTRANS_CLIENT_KEY // optional, kalau nanti mau dipakai di frontend
 });
 
 export function extractQrUrl(chargeResponse) {
